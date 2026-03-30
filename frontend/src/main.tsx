e8f6523541fdd060dc56f29763ab73874c954936
HawkinsWeb <128600888+crmhawkins@users.noreply.github.com>
2026-03-30 17:48:01 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.tsx'
 
+console.log('%c📧 Hawkins Mail v.05', 'color: #4CAF50; font-size: 16px; font-weight: bold;')
+
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
